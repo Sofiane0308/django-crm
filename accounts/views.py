8384848bc877d92725c809b1787febd4abda64d6
unknown <fs_chikhbled@esi.dz>
2020-09-22 23:44:18 +0200

    user registration

--- accounts/views.py
+++ accounts/views.py
@@ -1,13 +1,99 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from django.forms import inlineformset_factory
+from .models import *
+from .forms import OrderForm, CreateUserForm
+from .filters import OrderFilter
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib import messages
 
 # Create your views here.
 
+def registerPage(request):
+    form = CreateUserForm()
+
+    if request.method == 'POST':
+        form = CreateUserForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'account was created for ' + form.cleaned_data.get('username'))
+            return redirect('login')
+
+    context = {'form': form}
+    return render(request, 'accounts/register.html', context)
+
+def loginPage(request):
+    context = {}
+    return render(request, 'accounts/login.html', context)
+
 def home(request):
-    return render(request, 'accounts/dashboard.html')
+    orders = Order.objects.all()
+    customers = Customer.objects.all()
+
+    total_customers = customers.count()
+
+    total_orders = orders.count()
+    delivred = orders.filter(status='Delivred').count()
+    pending = orders.filter(status='Pending').count()
+
+    context = {'orders': orders,
+               'customers': customers,
+               'total_orders': total_orders,
+               'delivred': delivred,
+               'pending': pending
+               }
+
+    return render(request, 'accounts/dashboard.html', context)
+
 
 def products(request):
-    return render(request, 'accounts/products.html')
+    products = Product.objects.all()
+    return render(request, 'accounts/products.html', {'products': products})
 
-def customer(request):
-    return render(request, 'accounts/customer.html')
+
+def customer(request, pk):
+    customer = Customer.objects.get(id=pk)
+    orders = customer.order_set.all()
+    order_count = orders.count()
+
+    myFilter = OrderFilter(request.GET, queryset=orders)
+    orders = myFilter.qs
+    context = {'customer': customer, 'orders': orders, 'order_count':order_count, 'myFilter': myFilter}
+    return render(request, 'accounts/customer.html', context)
+
+def createOrder(request, pk):
+    OrderFormSet = inlineformset_factory(Customer, Order, fields=('product', 'status') )
+    customer = Customer.objects.get(id = pk)
+    formset = OrderFormSet(queryset = Order.objects.none(),instance=customer)
+    #form = OrderForm(initial={'customer': customer})
+    if request.method == 'POST':
+        #form = OrderForm(request.POST)
+        formset = OrderFormSet(request.POST, instance=customer)
+        if formset.is_valid():
+            formset.save()
+            return redirect('/')
+
+    context = {'formset': formset}
+    return render(request, 'accounts/order_form.html', context)
+
+def updateOrder(request, pk):
+    order = Order.objects.get(id=pk)
+    form = OrderForm(instance=order)
+    if request.method == 'POST':
+        form = OrderForm(request.POST, instance=order)
+        if form.is_valid():
+            form.save()
+            return redirect('/')
+    
+    context = {'form': form}
+    return render(request, 'accounts/order_form.html', context)
+
+def deleteOrder(request, pk):
+    order = Order.objects.get(id=pk)
+
+    if request.method == 'POST':
+        order.delete()
+        return redirect('/')
+
+    context={'item': order}
+    return render(request, 'accounts/delete.html', context)
