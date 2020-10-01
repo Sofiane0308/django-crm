bfed4dc79d130eb52445ec8645edc9323e7a10a6
unknown <fs_chikhbled@esi.dz>
2020-10-01 10:33:51 +0200

    pre deploy

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,3 +1,5 @@
+from django.contrib.auth import views as auth_views
+
 from django.urls import path
 from . import views
 
@@ -14,4 +16,9 @@ urlpatterns = [
     path('logout', views.logoutUser, name="logout"),
     path('user/', views.userPage, name='user-page'),
     path('account/', views.accountSettings, name='account'),
+
+    path('reset_password/', auth_views.PasswordResetView.as_view(template_name='accounts/password_reset.html'), name='reset_password'),
+    path('reset_password_sent/', auth_views.PasswordResetDoneView.as_view(template_name='accounts/password_reset_sent.html'), name="password_reset_done"),
+    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(template_name='accounts/password_reset_form.html'), name='password_reset_confirm'),
+    path('reset_password_complete/', auth_views.PasswordResetCompleteView.as_view(template_name='accounts/password_reset_done.html'), name="password_reset_complete"),
 ]
--- crm/settings.py
+++ crm/settings.py
@@ -130,3 +130,10 @@ STATICFILES_DIRS = [
 ]
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'static/images')
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'mydjangocrm@gmail.com'
+EMAIL_HOST_PASSWORD = '360Gamer'
